d73f8aa2478a30ec233bea30dd33fc07b45d236f
Eunike Jeannyfer <ceunikejeannyfer@gmail.com>
2023-10-27 23:15:23 +0700

    api

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("Hello Git")
-console.log("test")
+// const os = require('os');
+// const luasSegitiga = require('./segitiga.js')
+
+// console.log("Free Memory", os.freemem());
+// console.log(luasSegitiga(4,2))
+
+//HTTP RESPONSE
+
+const express = require('express')
+const app = express()
+const port = 3000
+const routers = require('./router')
+
+//untuk handle post req.body 
+app.use(express.json())
+
+app.use(routers)
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.listen(port, () => 
+    console.log(`Example app listening at http://localhost:${port}`))
