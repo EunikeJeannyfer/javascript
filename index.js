af0ea92f27f116b4d6f03d8045896e16ed37abf1
Eunike Jeannyfer <ceunikejeannyfer@gmail.com>
2023-10-31 20:39:28 +0700

    api documentation

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// const os = require('os');
-// const luasSegitiga = require('./segitiga.js')
-
-// console.log("Free Memory", os.freemem());
-// console.log(luasSegitiga(4,2))
-
 //HTTP RESPONSE
 
 const express = require('express')
 const app = express()
 const port = 3000
 const routers = require('./router')
+const swaggerJSON = require('./openapi.json')
+const swaggerUI = require('swagger-ui-express')
 
-//untuk handle post req.body 
+//untuk handle post req.body (middleware)
 app.use(express.json())
 
 app.use(routers)
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(port, () => 
