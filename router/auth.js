a50b6f327054bf214d0a84ae5ab6b253ec8761c5
Eunike Jeannyfer <ceunikejeannyfer@gmail.com>
2023-11-08 00:07:10 +0700

    oauth

--- router/auth.js
+++ router/auth.js
@@ -4,6 +4,9 @@ const controller = require('../app/controller')
 const passport = require('../utils/passport')
 const { auth } = require('../utils/jwt')
 
+const passportOAUTH = require('../utils/oauth')
+
+//auth
 router.post('/v2/auth/login', controller.auth.login)
 router.post('/v2/auth/register', controller.auth.register)
 router.get('/v2/auth/whoami',auth, controller.auth.whoami)
@@ -22,6 +25,19 @@ router.post('/login', passport.authenticate('local', {
     successRedirect: '/dashboard',
     failureRedirect: '/login'
 }))
+
+router.get('/auth/google', 
+    passportOAUTH.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
+
+router.get('/auth/google/callback', 
+    passportOAUTH.authenticate('google', {
+        failureRedirect: '/login',
+        session: false
+    }), controller.auth.oauth
+)
 
 module.exports = router
 
