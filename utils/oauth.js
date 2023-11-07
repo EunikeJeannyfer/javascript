de76c3aba92d316fa5c6d4fb7f262d492828cd68
Eunike Jeannyfer <ceunikejeannyfer@gmail.com>
2023-11-07 20:28:07 +0700

    oauth

---
+++ utils/oauth.js
@@ -0,0 +1,37 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient
+
+const {
+    GOOGLE_CLIENT_ID, 
+    GOOGLE_SECRET_ID
+} = process.env
+
+passport.use(new GoogleStrategy({
+    clientID : GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_SECRET_ID,
+    callbackURL: "http://localhost:3000/google/auth/callback"
+},
+    async function(accessToken, refreshToken, profile, done){
+        console.log(profile)
+        try{
+            let user = await prisma.user.upsert({
+                where: {email: profile.emails[0].value},
+                update: {googleId: profile.id},
+                create: {
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleId: profile.id
+                }
+            })
+            done(null, user)
+        }
+        catch(err){
+            done(error, null)
+        }
+    }
+))
+
+module.exports = passport
